417ebb8089967892f79b3c06638cae79ca099206
MInh PHO DUC LE <minh.pdl@gmail.com>
2019-05-27 13:20:43 +0000

    Automatically backed up by Learn

---
+++ lib/current_player.rb
@@ -0,0 +1,14 @@
+board = ["O", " ", " ", " ", "X", " ", " ", " ", "X"]
+
+def turn_count(board)
+  counter = 0
+    if  board.each(board) do |znak| == "O" 
+    counter += 1
+    end
+  
+end
+
+
+def current_player
+  
+end
